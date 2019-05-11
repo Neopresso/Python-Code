bd14985f6506f913b4431afb254c7ad841e452e6
Rishav Roy <42682016+Neopresso@users.noreply.github.com>
2019-05-11 11:51:39 +0100

    Add files via upload

---
+++ Bubble sort.py
@@ -0,0 +1,25 @@
+#main function for bubble sorting
+def bubble_sorter(arr):
+    listLength= len(arr)
+    tempNum=0
+    #loop for first number
+    for n1 in range(listLength):
+        #print('n1={}'.format(arr[n1]))
+        #loop for second number
+        for n2 in range(0, listLength-n1-1):
+            #print('n2={}'.format(arr[n2]))
+            #checks whether first number is greater than second
+            if arr[n2]>arr[n2+1]:
+                #swaps the numbers if the first number is greater than the second
+                tempNum=arr[n2+1]
+                arr[n2+1]=arr[n2]
+                arr[n2]=tempNum
+
+
+#List of integers to be sorted
+arr = [64, 34, 111, 95, 22, 11, 90]
+bubble_sorter(arr)
+
+print(arr)
+            
+            
